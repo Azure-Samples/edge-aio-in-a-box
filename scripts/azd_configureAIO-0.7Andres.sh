a69a3d8bf2cedef4bc43136ebaabec06bbcf83cd
AndresPad <anpadill@microsoft.com>
2024-10-08 10:06:31 -0400

    Update Azure CLI version to 2.64.0 in azd_configureAIO-0.7Andres.sh script

--- scripts/azd_configureAIO-0.7Andres.sh
+++ scripts/azd_configureAIO-0.7Andres.sh
@@ -135,7 +135,7 @@ apt-cache policy azure-cli
 # Obtain the currently installed distribution
 AZ_DIST=$(lsb_release -cs)
 # Store an Azure CLI version of choice
-AZ_VER=2.63.0
+AZ_VER=2.64.0
 # Install a specific version
 sudo apt-get install azure-cli=${AZ_VER}-1~${AZ_DIST}
 
