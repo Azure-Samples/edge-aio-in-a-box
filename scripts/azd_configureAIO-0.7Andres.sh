25de139883bc0367d88497b9918f8c29ba36fe96
AndresPad <anpadill@microsoft.com>
2024-10-08 10:33:15 -0400

    Refactor azd_configureAIO-0.7Andres.sh script and update Azure CLI installation

--- scripts/azd_configureAIO-0.7Andres.sh
+++ scripts/azd_configureAIO-0.7Andres.sh
@@ -131,11 +131,32 @@ echo "source <(helm completion bash)" >> /home/$adminUsername/.bashrc
 #############################
 echo "Installing Azure CLI"
 #curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+# Install a specific version
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -sLS https://packages.microsoft.com/keys/microsoft.asc |
+  gpg --dearmor | sudo tee /etc/apt/keyrings/microsoft.gpg > /dev/null
+sudo chmod go+r /etc/apt/keyrings/microsoft.gpg
+
+AZ_DIST=$(lsb_release -cs)
+echo "Types: deb
+URIs: https://packages.microsoft.com/repos/azure-cli/
+Suites: ${AZ_DIST}
+Components: main
+Architectures: $(dpkg --print-architecture)
+Signed-by: /etc/apt/keyrings/microsoft.gpg" | sudo tee /etc/apt/sources.list.d/azure-cli.sources
+
+sudo apt-get update
+sudo apt-get install azure-cli
+
 apt-cache policy azure-cli
+
 # Obtain the currently installed distribution
 AZ_DIST=$(lsb_release -cs)
+
 # Store an Azure CLI version of choice
 AZ_VER=2.64.0
+
 # Install a specific version
 sudo apt-get install azure-cli=${AZ_VER}-1~${AZ_DIST}
 
@@ -162,3 +183,91 @@ az connectedk8s connect \
 #Arc for Kubernetes Extensions
 #############################
 echo "Configuring Arc for Kubernetes Extensions"
+az extension add -n k8s-configuration --yes
+az extension add -n k8s-extension --yes
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# Sleep for 60 seconds to allow the cluster to be fully connected
+sleep 60
+
+#############################
+#Azure IoT Operations
+#############################
+# Starting off the post deployment steps. The following steps are to deploy Azure IoT Operations components
+# Reference: https://learn.microsoft.com/en-us/azure/iot-operations/deploy-iot-ops/howto-prepare-cluster?tabs=ubuntu#create-a-cluster
+# Reference: https://learn.microsoft.com/en-us/cli/azure/iot/ops?view=azure-cli-latest#az-iot-ops-init
+echo "Deploy IoT Operations Components. These commands take several minutes to complete."
+
+#Increase user watch/instance limits:
+echo fs.inotify.max_user_instances=8192 | sudo tee -a /etc/sysctl.conf
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf
+#Increase file descriptor limit:
+echo fs.file-max = 100000 | sudo tee -a /etc/sysctl.conf 
+
+sudo sysctl -p
+
+#Use the az connectedk8s enable-features command to enable custom location support on your cluster.
+#This command uses the objectId of the Microsoft Entra ID application that the Azure Arc service uses.
+echo "Enabling custom location support on the Arc cluster"
+
+az connectedk8s enable-features -g $rg \
+    -n $arcK8sClusterName \
+    --custom-locations-oid $customLocationRPSPID \
+    --features cluster-connect custom-locations
+
+# Initial values for variables
+SCHEMA_REGISTRY="aiobxregistry"
+SCHEMA_REGISTRY_NAMESPACE="aiobxregistryns"
+
+# Generate random 3-character suffix (alphanumeric)
+SUFFIX=$(tr -dc 'a-z0-9' </dev/urandom | head -c 4)
+
+# Append the unique suffixes to the variables
+SCHEMA_REGISTRY="${SCHEMA_REGISTRY}${SUFFIX}"
+SCHEMA_REGISTRY_NAMESPACE="${SCHEMA_REGISTRY_NAMESPACE}${SUFFIX}"
+
+az extension add -name azure-iot-ops --upgrade --yes --allow-preview false
+
+echo "Create a schema registry which will be used by Azure IoT Operations components after the deployment and connects it to the Azure Storage account."
+# az iot ops schema registry create -g $rg -n $SCHEMA_REGISTRY --registry-namespace $SCHEMA_REGISTRY_NAMESPACE --sa-resource-id $(az storage account show --name $STORAGE_ACCOUNT -o tsv --query id) --sa-container schemas
+az iot ops schema registry create -g $rg -n $SCHEMA_REGISTRY --registry-namespace $SCHEMA_REGISTRY_NAMESPACE --sa-resource-id "${stgId}" --sa-container schemas
+
+echo "Prepare the cluster for Azure IoT Operations deployment."
+#az iot ops schema registry show --name aiobxregistry1 --resource-group aiobxap070-aioedgeai-rg -o tsv --query id
+#az iot ops init -g aiobxap070-aioedgeai-rg --cluster aiobmclusterap --sr-resource-id "/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxx/resourceGroups/aiobxap070-aioedgeai-rg/providers/Microsoft.DeviceRegistry/schemaRegistries/aiobxregistry1"
+az iot ops init -g $rg \
+    --cluster $arcK8sClusterName \
+    --sr-resource-id "$(az iot ops schema registry show --name $SCHEMA_REGISTRY --resource-group $rg -o tsv --query id)"
+
+echo "Deploy Azure IoT Operations."
+az iot ops create -g $rg \
+    --cluster $arcK8sClusterName \
+    --custom-location "${arcK8sClusterName}-cl-${SUFFIX}" \
+    -n "${arcK8sClusterName}-ops-instance"
+
+
+#############################
+#Arc for Kubernetes AML Extension
+#############################
+#https://learn.microsoft.com/en-us/azure/machine-learning/how-to-deploy-kubernetes-extension
+# allowInsecureConnections=True - Allow HTTP communication or not. HTTP communication is not a secure way. If not allowed, HTTPs will be used.
+# InferenceRouterHA=False       - By default, AzureML extension will deploy 3 ingress controller replicas for high availability, which requires at least 3 workers in a cluster. Set this to False if you have less than 3 workers and want to deploy AzureML extension for development and testing only, in this case it will deploy one ingress controller replica only.
+
+
+#############################
+#Deploy Namespace, InfluxDB, Simulator, and Redis
+#############################
+#Create a folder for Cerebral configuration files
+mkdir -p /home/$adminUsername/cerebral
+sleep 30
+
+#Apply the Cerebral namespace
+kubectl apply -f https://raw.githubusercontent.com/Azure/arc_jumpstart_drops/main/sample_app/cerebral_genai/deployment/cerebral-ns.yaml
+
+#Create a directory for persistent InfluxDB data
+sudo mkdir /var/lib/influxdb2
+sudo chmod 777 /var/lib/influxdb2
+
+#Deploy InfluxDB, Configure InfluxDB, and Deploy the Data Simulator
