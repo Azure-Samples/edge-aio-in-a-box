e44b21e8fa2f70bf4d4c2af6dfb0711f3616e041
AndresPad <anpadill@microsoft.com>
2024-10-08 22:46:49 -0400

    Refactor azd_configureAIO-0.7Andres.sh script and update Azure CLI installation

--- scripts/azd_configureAIO-0.7Andres.sh
+++ scripts/azd_configureAIO-0.7Andres.sh
@@ -303,7 +303,7 @@ kubectl apply -f https://raw.githubusercontent.com/Azure-Samples/edge-aio-in-a-b
 kubectl apply -f https://raw.githubusercontent.com/Azure-Samples/edge-aio-in-a-box/main/rag-on-edge/yaml/rag-web-workload-aio0p6-acrairstream.yaml
 
 #rag-on-edge-interface: an interface module to interact with web frontend and the backend components.
-# kubectl apply -f https://raw.githubusercontent.com/Azure-Samples/edge-aio-in-a-box/main/rag-on-edge/yaml/rag-interface-dapr-workload-aio0p6-acrairstream.yaml
+kubectl apply -f https://raw.githubusercontent.com/Azure-Samples/edge-aio-in-a-box/main/rag-on-edge/yaml/rag-interface-dapr-workload-aio0p6-acrairstream.yaml
 
 #rag-on-edge-vectorDB: a database to store the vectors. 
 # kubectl apply -f https://raw.githubusercontent.com/Azure-Samples/edge-aio-in-a-box/main/rag-on-edge/yaml/rag-vdb-dapr-workload-aio0p6-acr-airstream.yaml
