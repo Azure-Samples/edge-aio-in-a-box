370803c58b4f21e5a5b4503fc3f607a06876df33
AndresPad <anpadill@microsoft.com>
2024-10-09 14:12:29 -0400

    Refactor rag-web workload deployment and service

--- scripts/azd_configureAIO-0.7.sh
+++ scripts/azd_configureAIO-0.7.sh
@@ -300,7 +300,7 @@ kubectl get all -n azureml-workloads
 kubectl apply -f https://raw.githubusercontent.com/Azure-Samples/edge-aio-in-a-box/main/rag-on-edge/yaml/rag-mq-components-aio0p6.yaml
 
 #rag-on-edge-web: a web application to interact with the user to submit the search and generation query.
-kubectl apply -f https://raw.githubusercontent.com/Azure-Samples/edge-aio-in-a-box/main/rag-on-edge/yaml/rag-web-workload-aio0p6-acrairstream.yaml
+kubectl apply -f https://raw.githubusercontent.com/Azure-Samples/edge-aio-in-a-box/main/rag-on-edge/yaml/rag-web-workload-aio0p6-acrairstreamv2.yaml
 
 #rag-on-edge-interface: an interface module to interact with web frontend and the backend components.
 kubectl apply -f https://raw.githubusercontent.com/Azure-Samples/edge-aio-in-a-box/main/rag-on-edge/yaml/rag-interface-dapr-workload-aio0p6-acrairstream.yaml
